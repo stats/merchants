56daf90205295acde1cba23fd9209bdd8cf33b7f
Dan Curran <codetown@gmail.com>
2020-04-29 08:28:58 -0400

    Remove crafting model and allow each item to be craftable and store its information with the item. Removes a lot of duplication.

--- src/app/items/shared/item.model.ts
+++ src/app/items/shared/item.model.ts
@@ -1,7 +1,14 @@
+import { KeyCount } from "@common/key-count.model";
+
 export interface Item {
   name: string;
   description: string;
   key: string;
   imageKey: string;
   cost: number;
+  craftable: boolean;
+  craftGoldCost: number;
+  craftRequiredItems: KeyCount[];
+  craftConsumedItems: KeyCount[];
+  craftedCount: number;
 }
